c9e5f171803717cd9526a74bf20347232304011c
Anas Kaarov <anasbinkurban@gmail.com>
2025-02-14 19:14:57 +0600

    feat: added 3142. Check if Grid Satisfies Conditions

---
+++ 3142. Check if Grid Satisfies Conditions.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def satisfiesConditions(self, grid: List[List[int]]) -> bool:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if i + 1 < len(grid) and j < len(grid[0]) and grid[i][j] != grid[i + 1][j]:
+                    return False
+                if i < len(grid) and j + 1 < len(grid[0]) and grid[i][j] == grid[i][j + 1]:
+                    return False
+        return True
